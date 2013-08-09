6c1262d0f23f8da04ddfadb9be4309f10beea291
AKovtunov <1sasha.sasha1@gmail.com>
2013-08-09 18:28:17 +0300

    Fixed tabs

--- lib/bodymedia/heart_rate_service.rb
+++ lib/bodymedia/heart_rate_service.rb
@@ -1,15 +1,15 @@
 module Bodymedia
   class Client
 
-  	def get_heartrate_for_period(start_date, end_date, *args)
-  		if args.length == 0
-  			get_parsed_data "/heartrate/#{start_date}/#{end_date}"
-  		else
+    def get_heartrate_for_period(start_date, end_date, *args)
+      if args.length == 0
+        get_parsed_data "/heartrate/#{start_date}/#{end_date}"
+      else
         return get_parsed_data "/heartrate/day/#{start_date}/#{end_date}" if args.include?(:daily)
         return get_parsed_data "/heartrate/day/session/#{start_date}/#{end_date}" if args.include?(:session)
         return get_parsed_data "/heartrate/day/session/detail/#{start_date}/#{end_date}" if args.include?(:detailed)
-  		end
-  	end
+      end
+    end
 
     #http://api.bodymedia.com/v2/heartrate/[zones]/day/[zones]/session/[zones]/detail/[zones]/{startDate}/[{endDate}]
   end
--- lib/bodymedia/measurement_service.rb
+++ lib/bodymedia/measurement_service.rb
@@ -1,22 +1,22 @@
 module Bodymedia
   class Client
 
-  	def get_measurements(param, *dates)
-  		if dates.length == 0
-  			get_parsed_data "/measurement/#{param}"
-  		else
-        	get_parsed_data "/measurement/#{param}/#{dates[0]}"
-  		end
-  	end
-  	def get_current_measurements
-  		get_parsed_data "/measurement/current"
-  	end
+    def get_measurements(param, *dates)
+      if dates.length == 0
+        get_parsed_data "/measurement/#{param}"
+      else
+          get_parsed_data "/measurement/#{param}/#{dates[0]}"
+      end
+    end
+    def get_current_measurements
+      get_parsed_data "/measurement/current"
+    end
     #http://api.bodymedia.com/v2/heartrate/[zones]/day/[zones]/session/[zones]/detail/[zones]/{startDate}/[{endDate}]
   end
 end
-#GET, POST	/measurement/[WAISTLINE|WEIGHT]
+#GET, POST  /measurement/[WAISTLINE|WEIGHT]
 #The user's most recent weight or waistline measurement.
-#GET, POST, DELETE	/measurement/[WAISTLINE|WEIGHT]/{date}
+#GET, POST, DELETE  /measurement/[WAISTLINE|WEIGHT]/{date}
 #The user's weight or waistline measurement on the specified date.
-#GET	/measurement/current
+#GET  /measurement/current
 #The user's most recent weight and waistline measurements.
